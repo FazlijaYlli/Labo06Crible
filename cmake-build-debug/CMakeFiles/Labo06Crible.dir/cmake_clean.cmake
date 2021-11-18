13e2300bc17a6c9629a1f0d42a40695b5d37b9df
Ylli Fazlija <fazlijaylli@gmail.com>
2021-11-18 16:41:31 +0100

    Created CLion Project

---
+++ cmake-build-debug/CMakeFiles/Labo06Crible.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Labo06Crible.dir/main.cpp.obj"
+  "Labo06Crible.exe"
+  "Labo06Crible.exe.manifest"
+  "Labo06Crible.pdb"
+  "libLabo06Crible.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Labo06Crible.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Labo06Crible.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Labo06Crible.
+# This may be replaced when dependencies are built.
